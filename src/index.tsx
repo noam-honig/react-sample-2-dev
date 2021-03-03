0f2aadad59bcc9bb08a685d073e946bbc560d6e8
Noam <noam@fireflymigration.com>
2021-03-03 17:47:50 +0200

    added routes and error handling

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+window.onerror = function (msg, src, lineno, colno, error) { alert(msg) }
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
