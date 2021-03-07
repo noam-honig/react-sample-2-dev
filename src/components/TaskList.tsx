16d650a965a40c619cc4b1170e751acd68213103
Noam <noam@fireflymigration.com>
2021-03-07 17:54:33 +0200

    task list can add task and get data from server

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,9 +1,35 @@
 import { Component } from "react";
+import { Tasks } from "../tasks/tasks";
+import { context } from '../common';
 
 class TaskList extends Component {
+    tasks: Tasks[] = [];
+    async loadTasks() {
+        this.tasks = await context.for(Tasks).find();
+        this.setState({});
+    }
+    componentDidMount() {
+        this.loadTasks();
+    }
+    async addTask() {
+        let t = context.for(Tasks).create();
+        t.name.value = this.newTaskName;
+        await t.save();
+        this.newTaskName = '';
+        this.loadTasks();
+    }
+    newTaskName: string = '';
+
     render() {
         return <div>
-            Hello Tasks
+            <h3>Tasks</h3>
+            <input value={this.newTaskName} onChange={x => { this.newTaskName = x.target.value; this.setState({}) }}></input>
+            <button onClick={() => this.addTask()}>Add Task</button>
+            <ul>
+                {this.tasks.map(t => (
+                    <li key={t.id.value}>{t.name.value}
+                    </li>))}
+            </ul>
         </div>
     }
 }
