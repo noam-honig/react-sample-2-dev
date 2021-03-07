1ebc5fc6b699e226930e268283efda55e3bed68e
Noam <noam@fireflymigration.com>
2021-03-07 17:41:27 +0200

    Added TaskList

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route, Link } from "react-router-dom";
 import './App.css';
+import TaskList from './components/TaskList';
 
 function App() {
   return (
     <div className="App">
       <h1>Remult with React</h1>
       <Switch>
+        <Route exact path={["/", "/tasks"]} component={TaskList} />
       </Switch>
     </div>
   );
---
+++ src/components/TaskList.tsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class TaskList extends Component {
+    render() {
+        return <div>
+            Hello Tasks
+        </div>
+    }
+}
+export default TaskList;
